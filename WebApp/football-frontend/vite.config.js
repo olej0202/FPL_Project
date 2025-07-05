e39588acf10a59716135b96079c5a1244da7face
Ole Jacob Simensen <ole.j.simensen@twoday.com>
2025-07-05 02:17:57 +0200

    Add lucide-react dependency

--- WebApp/football-frontend/vite.config.js
+++ WebApp/football-frontend/vite.config.js
@@ -4,10 +4,10 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  publicDir: 'public', // 👈 Add this line
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
 })
-
