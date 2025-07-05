92cbada9828d0694bb874cfa5e51c75095931ce4
Ole Jacob Simensen <ole.j.simensen@twoday.com>
2025-07-05 02:33:46 +0200

    Add lucide-react dependency

--- WebApp/football-frontend/vite.config.js
+++ WebApp/football-frontend/vite.config.js
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  plugins: [react()],
-  publicDir: 'public', // 👈 Add this line
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/_redirects', // source file
+          dest: '.'                // destination in dist/
+        }
+      ]
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
