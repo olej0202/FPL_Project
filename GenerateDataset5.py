235a505eb3dce2b8b5cb4264cbfbd4c7b5568961
Ole Jacob Simensen <ole.j.simensen@twoday.com>
2025-07-29 22:13:57 +0200

    se

--- GenerateDataset5.py
+++ GenerateDataset5.py
@@ -744,9 +744,9 @@ def team_transformed2():
 
 
 
-        k_def = 0.07
-        k_off=0.07
-        min_val=0.5
+        k_def = 0.06
+        k_off=0.06
+        min_val=0.6
 
         actual_goals = xg
         if was_home==1:
@@ -1205,7 +1205,7 @@ def adjust_measure(df, measure_name):
     new_expected_goals=[]
     current_expected_goals_start_value=player_df[measure_name].mean()
     current_expected_goals=current_expected_goals_start_value
-    smoothing_f=0.08
+    smoothing_f=0.05
     min_val=std
     count=0
     in_row=0
