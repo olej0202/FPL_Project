9b0e29e078f4884e3f7e55429c992c8ab11fee81
Ole Jacob Simensen <ole.j.simensen@twoday.com>
2025-08-25 17:42:30 +0200

    se

--- GenerateConfig.py
+++ GenerateConfig.py
@@ -25,7 +25,7 @@ Spurs,6
 ,Ipswich,40"""
 
 
-Manual_team_offensive_adjustments = {94:0.9,1:1.2,31:0.9,21:0.9,4:0.9 }
+Manual_team_offensive_adjustments = {94:0.9,1:1.1,31:0.9,21:0.9,4:0.9 }
     
     
     
@@ -40,15 +40,13 @@ Manual_NewPlayer_Adjustments={
         "El_Hadji Malick Diouf":["Aaron_Wan-Bissaka", "Lucas_Digne"], 
         "Jeremie_Frimpong":["Andrew_Robertson","Pedro_Porro","Lucas_Digne"],
         "Benjamin_Sesko":["Kai_Havertz","Rasmus_Højlund","Marcus_Rashford"], 
-        "Hugo_Ekitiké":["Kai_Havertz","Yoane_Wissa","Alexander_Isak" ]    
+        "Hugo_Ekitiké":["Kai_Havertz","Alexander_Isak" ]    
     }
 
 Manual_Player_Adjustments={
         "Mohammed_Kudus":["Brennan_Johnson","Son_Heung-min"],
         "Matheus_Santos Carneiro Da Cunha":["Bruno_Borges Fernandes","Alejandro_Garnacho Ferreyra","Amad_Diallo"],
         "Bryan_Mbeumo":["Bruno_Borges Fernandes","Alejandro_Garnacho Ferreyra","Amad_Diallo"],
-        "Cole_Palmer":["Mohamed_Salah","Bukayo_Saka"],
-        "Ollie_Watkins":["Erling_Haaland","Yoane_Wissa"],
         "Anthony_Gordon":["Alexander_Isak","Jacob_Murphy","Harvey_Barnes"],   
         "Igor_Thiago Nascimento Rodrigues":["Yoane_Wissa","Bryan_Mbeumo"],
         "Lucas_Tolentino Coelho de Lima":["Jarrod_Bowen"],
--- Generate_Optimize_Myteam.py
+++ Generate_Optimize_Myteam.py
@@ -102,7 +102,7 @@ def get_my_team(team_id=46805,Last_GW=1):
             gameweek = Last_GW  # Replace with the desired gameweek
     except:
         df=pd.DataFrame()
-        saved_transfers=min(5,Last_GW)
+        saved_transfers=min(5,Last_GW)-1
 
         initial_saved=saved_transfers+hit
 
@@ -155,10 +155,10 @@ def optimize_my_team(team_id=7025308,wildcard_round=40, bb_round=40,Last_GW=2,ba
 
     team_id=team_id
 
-    wildcard_round = wildcard_round  # Gameweek 3 (Index t=2)
+    wildcard_round = wildcard_round-Last_GW  # Gameweek 3 (Index t=2)
     if(is_first):
         wildcard_round = 1  # Gameweek 3 (Index t=2)
-    bench_points_gw=bb_round
+    bench_points_gw=bb_round-Last_GW
     Last_GW=Last_GW
     hit=0
     banned_list=banned_list
