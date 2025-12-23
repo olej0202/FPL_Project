574eaa4cbc65214edeced8c7deb8bafe69d5f50b
Ole Jacob Simensen <ole.j.simensen@twoday.com>
2025-12-23 19:51:55 +0100

    se

--- GenerateOrchestration.py
+++ GenerateOrchestration.py
@@ -39,14 +39,14 @@ class DeepNN(nn.Module):
 
 
 def Data_Extraction(season,is_new_season,has_been_error):
-    #main_Extract(season, is_new_season, has_been_error)
+    main_Extract(season, is_new_season, has_been_error)
     current_players(season)
     current_teams(season)
-    #main_Extract_Understat(season)
+    main_Extract_Understat(season)
 
 
 def Data_Transformation(n_points_in_future, current_fixture_path,current_player_path,current_team_path,time_list,run_player_pos):
-    main_Transform()
+    #main_Transform()
     Generate_Understat_dataset(current_player_path,run_player_pos)
     team_data(current_team_path)
     GetXmins(current_player_path, time_list, scenarios=Manual_min)
@@ -89,7 +89,7 @@ def Get_times(current_fixture_path,n_points_in_future):
 def Main_Orchestration():
     season=25
     is_new_season=0
-    has_been_error=1
+    has_been_error=0
     n_points_in_future=8
     budget=101
     ownership=0.9
@@ -110,7 +110,7 @@ def Main_Orchestration():
     
     
     #EXTARCT DATA
-    Data_Extraction(season,is_new_season,has_been_error)
+    #Data_Extraction(season,is_new_season,has_been_error)
     
     
     #Transform data
