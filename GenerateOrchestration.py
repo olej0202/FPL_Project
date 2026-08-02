a29905390bab501af9082141be0507e448768e1a
Ole Jacob Simensen <ole.j.simensen@twoday.com>
2026-08-02 13:19:01 +0200

    se

--- GenerateOrchestration.py
+++ GenerateOrchestration.py
@@ -324,7 +324,7 @@ def Main_Orchestration():
     
     
     #Transform data
-    #Data_Transformation(n_points_in_future, current_fixture_path,current_player_path,current_team_path,time_list,run_player_pos,Understat_path,Understat_shots_path)
+    Data_Transformation(n_points_in_future, current_fixture_path,current_player_path,current_team_path,time_list,run_player_pos,Understat_path,Understat_shots_path)
     
     #Predict data
     Data_Predictions(
