bfcc176553bb0976b4958421e042fe99a39f4a29
Ole Jacob Simensen <ole.j.simensen@twoday.com>
2026-08-24 07:52:58 +0200

    se

--- GenerateOrchestration.py
+++ GenerateOrchestration.py
@@ -112,11 +112,11 @@ def Data_Extraction(season,is_new_season,has_been_error):
 
 
 def Data_Transformation(n_points_in_future, current_fixture_path,current_player_path,current_team_path,time_list,run_player_pos,Understat_path,Understat_shots_path):
-    main_Transform()
-    Generate_Understat_dataset(current_player_path,run_player_pos)
-    Generate_Shots_data(Understat_path,Understat_shots_path,current_player_path,current_team_path)
-    team_data(current_team_path)
-    GetXmins(current_player_path, time_list, scenarios=Manual_min)
+    #main_Transform()
+    #Generate_Understat_dataset(current_player_path,run_player_pos)
+    #Generate_Shots_data(Understat_path,Understat_shots_path,current_player_path,current_team_path)
+    #team_data(current_team_path)
+    #GetXmins(current_player_path, time_list, scenarios=Manual_min)
     GeneratePlayerData(time_list, current_fixture_path,current_player_path,current_team_path)
 
     
@@ -176,7 +176,7 @@ def Data_Predictions(
         optimization_best_output_path=sim_output_dir / "simtest_parameter_best.csv",
         write_outputs=True,
     )
-    """
+    
     print("Running full simulator parameter optimization...")
     run_simulator_control(control_cfg=full_sim_control)
 
@@ -260,7 +260,7 @@ def Data_Predictions(
         len(gw_list),
         time_list=gw_list,
         standings_fixture_path=str(fixture_path),
-    )"""
+    )
     Make_Predictions()
     Generate_point_predictions(time_list)
     #make_predictions2(horizon=len(time_list), gw_list=time_list)
@@ -328,7 +328,7 @@ def Main_Orchestration():
     
     
     #Transform data
-    #Data_Transformation(n_points_in_future, current_fixture_path,current_player_path,current_team_path,time_list,run_player_pos,Understat_path,Understat_shots_path)
+    Data_Transformation(n_points_in_future, current_fixture_path,current_player_path,current_team_path,time_list,run_player_pos,Understat_path,Understat_shots_path)
     
     #Predict data
     Data_Predictions(
